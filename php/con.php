03f7cd98a66f39dfa8de7f4b4ba32fb3bbefe08d
Arnaud Crowther <arnaudcrowther@gmail.com>
2015-04-05 14:04:35 -0400

    Delete con.php

--- php/con.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbHost = "localhost";
-$dbUser = "ours";
-$dbPass = "#OUrs!2014";
-$dbDatabase = "crawler";
-
-$db = mysql_connect($dbHost, $dbUser, $dbPass)or die("Error connecting to database.");
-mysql_select_db($dbDatabase, $db)or die("Couldn't select the database.");
